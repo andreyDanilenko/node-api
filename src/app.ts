a875b3ac06220bb77a5d32cab4527617764ff09c
adanilenko <a.danilenko@platim.ru>
2023-04-03 06:20:29 +0300

    exception filter

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import express, { Express } from 'express';
-import { userRouter } from './users/users';
 import { Server } from 'http'
+import { ExceptionFilter } from './errors/exception.filter';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
 
@@ -10,22 +10,32 @@ export class App {
     port: number;
     logger: LoggerService;
     userController: UserController;
+    exceptionFilter: ExceptionFilter;
 
-    constructor(logger: LoggerService,
+
+    constructor(
+        logger: LoggerService,
         userController: UserController,
+        exceptionFilter: ExceptionFilter
     ) {
         this.app = express();
         this.port = 8000;
         this.logger = logger
         this.userController = userController
+        this.exceptionFilter = exceptionFilter
     }
 
     useRoutes() {
         this.app.use('/users', this.userController.router)
     }
 
+    useExceptionFilters() {
+        this.app.use(this.exceptionFilter.catch.bind(this.exceptionFilter))
+    }
+
     public async init() {
         this.useRoutes();
+        this.useExceptionFilters();
         this.server = this.app.listen(this.port);
         this.logger.log(`Server stated http://localhost:${this.port}`);
     }
