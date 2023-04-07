b223ebf2e13b85a5514149f4f37840efaac77ccb
adanilenko <a.danilenko@platim.ru>
2023-04-07 09:37:16 +0300

    add logger interface

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express';
 import { Server } from 'http'
 import { ExceptionFilter } from './errors/exception.filter';
+import { ILogger } from './logger/logger.interface';
 import { LoggerService } from './logger/logger.service';
 import { UserController } from './users/users.controller';
 
@@ -8,13 +9,13 @@ export class App {
     app: Express;
     server: Server
     port: number;
-    logger: LoggerService;
+    logger: ILogger;
     userController: UserController;
     exceptionFilter: ExceptionFilter;
 
 
     constructor(
-        logger: LoggerService,
+        logger: ILogger,
         userController: UserController,
         exceptionFilter: ExceptionFilter
     ) {
