e9fe652b8b3ad8fd1d073d5170e5ff95c84d3d8a
adanilenko <a.danilenko@platim.ru>
2023-04-11 11:09:42 +0300

    test metadata

--- src/testMeta.ts
+++ src/testMeta.ts
@@ -7,11 +7,34 @@ function Test(target: Function) {
 
 }
 
-function Prop (target: Object, name: string) {
-    
+function Injectable(key: string) {
+    return (target: Function) => {
+        Reflect.defineMetadata('a', 1, target)
+        const meta = Reflect.getMetadata('a', target)
+        console.log(meta);
+    }
 }
 
-@Test
+function Inject(key: string) {
+    return (target: Function) => {
+        Reflect.defineMetadata('a', 1, target)
+        const meta = Reflect.getMetadata('a', target)
+        console.log(meta);
+    }
+}
+
+function Prop(target: Object, name: string) {
+
+}
+
+@Injectable('C')
 export class C {
- @Prop prop: number;
+    @Prop prop: number;
+}
+
+@Injectable('D')
+export class D {
+    constructor(@Inject('C') c: C) {
+
+    }
 }
