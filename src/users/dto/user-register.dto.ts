97185ac316fc4c6319a9c9553c85ee28728b84a8
adanilenko <a.danilenko@platim.ru>
2023-04-20 19:34:24 +0300

    add package class validate

--- src/users/dto/user-register.dto.ts
+++ src/users/dto/user-register.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsString } from 'class-validator';
 export class UserRegisterDto {
+	@IsEmail({}, { message: 'Wrong email specified' })
 	email: string;
+
+	@IsString({ message: 'No password specified' })
 	password: string;
+
+	@IsString({ message: 'No name specified' })
 	name: string;
 }
