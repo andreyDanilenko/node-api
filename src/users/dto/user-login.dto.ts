16081049330dde0b955b743114570123984aac58
adanilenko <a.danilenko@platim.ru>
2023-06-14 19:47:04 +0200

    add types

--- src/users/dto/user-login.dto.ts
+++ src/users/dto/user-login.dto.ts
@@ -1,4 +1,8 @@
+import { IsEmail, IsString } from 'class-validator';
 export class UserLoginDto {
+	@IsEmail({}, { message: 'Wrong email specified' })
 	email: string;
+
+	@IsString({ message: 'No password specified' })
 	password: string;
 }
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,10 +1,18 @@
 // Used to encapsulate business logic associated with a real object
 // Isolated business logic of individual entities
-import { hash } from 'bcryptjs';
+import { compare, hash } from 'bcryptjs';
 
 export class User {
 	private _password: string;
-	constructor(private readonly _email: string, private readonly _name: string) {}
+	constructor(
+		private readonly _email: string,
+		private readonly _name: string,
+		passwordHash?: string,
+	) {
+		if (passwordHash) {
+			this._password = passwordHash;
+		}
+	}
 
 	get email(): string {
 		return this._email;
@@ -20,5 +28,9 @@ export class User {
 
 	public async setPassword(pass: string, salt: number): Promise<void> {
 		this._password = await hash(pass, salt);
+	}
+
+	public async comparePassword(pass: string): Promise<boolean> {
+		return compare(pass, this._password);
 	}
 }
