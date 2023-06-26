ba44ad1fbb5bcf2af94a8618013313974b4b7ec3
adanilenko <a.danilenko@platim.ru>
2023-06-26 11:54:47 +0200

    Init unit test for users controller

---
+++ src/users/user.service.spec.ts
@@ -0,0 +1,34 @@
+import { Container } from 'inversify';
+import { IConfigService } from '../config/config.service.interface';
+import { TYPES } from '../types';
+import { IUsersRepository } from './users.repository.interface';
+import { UserService } from './users.service';
+import { IUserService } from './users.service.interface';
+
+const ConfigServiceMock: IConfigService = {
+	get: jest.fn(),
+};
+
+const UserRepositoryMock: IUsersRepository = {
+	find: jest.fn(),
+	create: jest.fn(),
+};
+
+const container = new Container();
+let configService: IConfigService;
+let usersRepository: IUsersRepository;
+let usersService: IUserService;
+
+beforeAll(() => {
+	container.bind<IUserService>(TYPES.UserService).to(UserService);
+	container.bind<IConfigService>(TYPES.ConfigService).toConstantValue(ConfigServiceMock);
+	container.bind<IUsersRepository>(TYPES.UsersRepository).toConstantValue(UserRepositoryMock);
+
+	configService = container.get<IConfigService>(TYPES.ConfigService);
+	usersRepository = container.get<IUsersRepository>(TYPES.UsersRepository);
+	usersService = container.get<IUserService>(TYPES.UserService);
+});
+
+describe('User service', () => {
+	it('createUser', async () => {});
+});
